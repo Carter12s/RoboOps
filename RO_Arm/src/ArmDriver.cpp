f66b146e20618763ccf2292aa7e445cbf17f15cc
Carter <carter12s@msn.com>
2015-04-19 16:59:39 -0600

    Arm driver auto engaged code added

--- RO_Arm/src/ArmDriver.cpp
+++ RO_Arm/src/ArmDriver.cpp
@@ -24,6 +24,34 @@ float theta2_max = 90.0f;
 float theta3_min = -44.0f;
 float theta3_max = 0.0f;
 
+void engageArm(){
+	corobot_srvs::SetEngaged srv;
+	srv.request.index = 0;
+	srv.request.state = 1;
+	servo_setEngaged.call(srv);
+	srv.request.index = 1;
+	servo_setEngaged.call(srv);
+	srv.request.index = 2;
+	servo_setEngaged.call(srv);
+	srv.request.index = 3;
+	servo_setEngaged.call(srv);
+	armEngaged = true;
+}
+
+void disengageArm(){
+	corobot_srvs::SetEngaged srv;
+	srv.request.index = 0;
+	srv.request.state = 0;
+	servo_setEngaged.call(srv);
+	srv.request.index = 1;
+	servo_setEngaged.call(srv);
+	srv.request.index = 2;
+	servo_setEngaged.call(srv);
+	srv.request.index = 3;
+	servo_setEngaged.call(srv);
+	armEngaged = false;
+}
+
 bool limitAngles(RO_srv::Arm_setAngles::Request &req){
 	bool passed = true;
 	if(!(req.theta0 >= theta0_min && req.theta0 <= theta0_max)){
@@ -45,7 +73,15 @@ bool limitAngles(RO_srv::Arm_setAngles::Request &req){
 	return passed;	
 }
 
-bool engageArm(RO_srv::Arm_engage::Request &req, RO_srv::Arm_engage::Response &res){
+bool setEngaged(RO_srv::Arm_engage::Request &req, RO_srv::Arm_engage::Response &res){
+	if(armEngaged){
+		ROS_INFO("Arm already engaged re-engaging?");
+	}
+	if(req.state){
+		engageArm();
+	}else{
+		disengageArm();
+	}	
 	return false;
 }
 
@@ -58,7 +94,10 @@ bool homeArm(RO_srv::Arm_home::Request &req, RO_srv::Arm_home::Response &res){
 bool setAngles(RO_srv::Arm_setAngles::Request &req, RO_srv::Arm_setAngles::Response &res){
  	ROS_INFO("Arm_setAngles Called: %f %f %f %f",req.theta0, req.theta1,req.theta2,req.theta3);
 	corobot_srvs::SetPosition srv;
-		
+	if(!armEngaged){
+		ROS_WARN("Arm not engaged, automatically engaging");
+		engageArm();
+	}	
 	if(limitAngles(req)){
 	srv.request.index = 0;
         srv.request.position = req.theta0*440.0f/90.0f+1440.0f;
@@ -92,6 +131,7 @@ int main(int argc, char **argv){
 	ros::init(argc,argv,"ArmDriver");	
 	ros::NodeHandle n;
 	ros::ServiceServer service = n.advertiseService("Arm_setAngles",setAngles);
+	ros::ServiceServer service2 = n.advertiseService("Arm_setEngaged",setEngaged);
 	servo_setEngaged = n.serviceClient<corobot_srvs::SetEngaged>("phidgetServo_setEngaged");
 	servo_setPosition = n.serviceClient<corobot_srvs::SetPosition>("phidgetServo_setPosition");
 	ROS_INFO("Arm Driver Initialized");
